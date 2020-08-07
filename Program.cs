92fe067a862fee6cddb1f351acf26345ef17b5b0
AmorFatiBA <you@example.com>
2020-08-07 14:59:35 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace deneme18_VirtualMetod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Mammal mammal1=new Mammal(25,36);
+           Cat cat1 = new Cat("Bengal Kedisi", 28,3);
+           Sheep sheep1 = new Sheep("Merinos", 55,30);
+
+           mammal1=cat1;
+           mammal1.Speak();
+
+           mammal1=sheep1;
+           mammal1.Speak();
+        }
+    }
+}
---
+++ VirtualMetod.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace deneme18_VirtualMetod
+{
+    class Mammal
+    {
+        public double Height
+        {
+            get;
+            set;
+        }
+        public double Weight
+        {
+            get;
+            set;
+        }
+        public Mammal(double height, double weight)
+        {
+            this.Height = height;
+            this.Weight = weight;
+        }
+        virtual public void Speak()
+        {
+            Console.WriteLine("I don't speak.");
+        }
+
+    }
+    class Cat : Mammal
+    {
+        public string Species
+        {
+            get;
+            set;
+        }
+        public Cat(string species, int height, int weight) : base(height, weight)
+        {
+            this.Species = species;
+        }
+        override public void Speak()
+        {
+            Console.WriteLine("I'm a cat!\n");
+        }
+    }
+    class Sheep : Mammal
+    {
+        public string Species
+        {
+            get;
+            set;
+        }
+        public Sheep(string species, int height, int weight) : base(height, weight)
+        {
+            this.Species = species;
+        }
+        override public void Speak()
+        {
+            Console.WriteLine("I'm a sheep!\n");
+        }
+    }
+}
